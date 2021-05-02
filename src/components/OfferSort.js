48366b4ee8e49929931f1d5cfc181acecc659f3d
Paddy Bhambare <pradhyumbhambare2@gmail.com>
2021-05-02 12:41:25 +0530

    offerSort feature completed and add distance field in offer card

--- src/components/OfferSort.js
+++ src/components/OfferSort.js
@@ -31,6 +31,107 @@ const offerSort = (props) => {
 
     })
 
+    const distanceSort = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.distance > b.distance) ? 1 : -1)
+        props.setCurrentOffers(sortOffers)
+        props.toggleModal()
+    }
+
+    const distanceSortReverse = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.distance > b.distance) ? 1 : -1)
+        props.setCurrentOffers(sortOffers.reverse())
+        props.toggleModal()
+    }
+
+    const startDateSort = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.start_date > b.start_date) ? 1 : -1)
+        props.setCurrentOffers(sortOffers)
+        props.toggleModal()
+    }
+
+    const startDateSortReverse = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.start_date > b.start_date) ? 1 : -1)
+        props.setCurrentOffers(sortOffers.reverse())
+        props.toggleModal()
+    }
+
+    const endDateSort = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.end_date > b.end_date) ? 1 : -1)
+        props.setCurrentOffers(sortOffers)
+        props.toggleModal()
+    }
+
+    const endDateSortReverse = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.end_date > b.end_date) ? 1 : -1)
+        props.setCurrentOffers(sortOffers.reverse())
+        props.toggleModal()
+    }
+
+    const postDateSort = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.post_time > b.post_time) ? 1 : -1)
+        props.setCurrentOffers(sortOffers.reverse())
+        props.toggleModal()
+    }
+
+    const offerTitleSort = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.offer_title > b.offer_title) ? 1 : -1)
+        props.setCurrentOffers(sortOffers)
+        props.toggleModal()
+    }
+
+    const offerTitleSortReverse = () => {
+        var sortOffers = props.currentOffers.sort((a, b) => (a.offer_title > b.offer_title) ? 1 : -1)
+        props.setCurrentOffers(sortOffers.reverse())
+        props.toggleModal()
+    }
+
+    const onGoingSort = () => {
+        var onGoingOffers = []
+        var today = new Date();
+        var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        props.currentOffers.forEach((element) => {
+            var start_date = new Date(element.start_date);
+            var start_date = start_date.getFullYear() + '-' + (start_date.getMonth() + 1) + '-' + start_date.getDate()
+            var end_date = new Date(element.end_date);
+            var end_date = end_date.getFullYear() + '-' + (end_date.getMonth() + 1) + '-' + end_date.getDate()
+            if (start_date == currentDate || end_date >= currentDate)
+                onGoingOffers.push(element)
+        })
+        props.setCurrentOffers(onGoingOffers)
+        props.toggleModal()
+    }
+
+    const exporingSoonSort = () => {
+        var exporingSoonOffers = []
+        var newDay = new Date(today.getFullYear(), today.getMonth(), 8);
+        var lastWeekDay = newDay.getFullYear() + '-' + (newDay.getMonth() + 1) + '-' + newDay.getDate();
+        props.currentOffers.forEach((element) => {
+            var start_date = new Date(element.start_date);
+            var start_date = start_date.getFullYear() + '-' + (start_date.getMonth() + 1) + '-' + start_date.getDate()
+            var end_date = new Date(element.end_date);
+            var end_date = end_date.getFullYear() + '-' + (end_date.getMonth() + 1) + '-' + end_date.getDate()
+            if (end_date <= lastWeekDay)
+                exporingSoonOffers.push(element)
+        })
+        props.setCurrentOffers(exporingSoon)
+        props.toggleModal()
+    }
+
+    const todaySort = () => {
+        var todaysOffers = []
+        var today = new Date();
+        var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        props.currentOffers.forEach((element) => {
+            var start_date = new Date(element.start_date);
+            var start_date = start_date.getFullYear() + '-' + (start_date.getMonth() + 1) + '-' + start_date.getDate()
+            var end_date = new Date(element.end_date);
+            var end_date = end_date.getFullYear() + '-' + (end_date.getMonth() + 1) + '-' + end_date.getDate()
+            if (start_date == currentDate || end_date == currentDate)
+                todaysOffers.push(element)
+        })
+        props.setCurrentOffers(todaysOffers)
+        props.toggleModal()
+    }
 
     return (
         <Modal
@@ -70,25 +171,25 @@ const offerSort = (props) => {
                         <SortToggleButton
                             value="On Going"
                             onPress={(value) => {
-                                console.log(value)
+                                onGoingSort()
                             }}
                         />
                         <SortToggleButton
                             value="Title : A to Z"
                             onPress={(value) => {
-                                console.log(value)
+                                offerTitleSort()
                             }}
                         />
                         <SortToggleButton
                             value="Title : Z to A"
                             onPress={(value) => {
-                                console.log(value)
+                                offerTitleSortReverse()
                             }}
                         />
                         <SortToggleButton
                             value="Expiring Soon"
                             onPress={(value) => {
-                                console.log(value)
+                                exporingSoonSort()
                             }}
                         />
                     </View>
@@ -99,37 +200,37 @@ const offerSort = (props) => {
                         <SortToggleButton
                             value="Today"
                             onPress={(value) => {
-                                console.log(value)
+                                todaySort()
                             }}
                         />
                         <SortToggleButton
                             value={ButtonNames.start_date_asc}
                             onPress={(value) => {
-                                console.log(value)
+                                startDateSort()
                             }}
                         />
                         <SortToggleButton
                             value={ButtonNames.start_date_desc}
                             onPress={(value) => {
-                                console.log(value)
+                                startDateSortReverse()
                             }}
                         />
                         <SortToggleButton
                             value={ButtonNames.end_date_asc}
                             onPress={(value) => {
-                                console.log(value)
+                                endDateSort()
                             }}
                         />
                         <SortToggleButton
                             value={ButtonNames.end_date_desc}
                             onPress={(value) => {
-                                console.log(value)
+                                endDateSortReverse()
                             }}
                         />
                         <SortToggleButton
                             value="Post Date"
                             onPress={(value) => {
-                                console.log(value)
+                                postDateSort()
                             }}
                         />
 
@@ -139,15 +240,12 @@ const offerSort = (props) => {
                         <View style={{ width: "90%", marginLeft: 20, flexWrap: 'wrap', flexDirection: 'row' }}>
                             <SortToggleButton
                                 value="Min to Max"
-                                onPress={(value) => {
-                                    console.log(value)
-                                }}
+                                onPress={(value) => { distanceSort() }}
+
                             />
                             <SortToggleButton
                                 value="Max to Min"
-                                onPress={(value) => {
-                                    console.log(value)
-                                }}
+                                onPress={(value) => { distanceSortReverse() }}
                             />
 
                         </View>
